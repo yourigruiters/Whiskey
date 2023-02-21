ede4442bceddbcede16dbd1d107eb9b12b5f32c4
Youri Gruiters <i-youri-i@hotmail.com>
2023-02-21 10:12:32 +0100

    docs(tailwind): add correct colors to theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,32 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      islands: {
+        default: "#D71E44",
+        hover: "#9E1B34",
+      },
+      islay: {
+        default: "#A5238E",
+        hover: "#6E2671",
+      },
+      highlands: {
+        default: "#563494",
+        hover: "#322773",
+      },
+      lowlands: {
+        default: "#0663AE",
+        hover: "#174272",
+      },
+      speyside: {
+        default: "#1AB467",
+        hover: "#007B46",
+      },
+      campbeltown: {
+        default: "#F3B41B",
+        hover: "#D29D2A",
+      },
+    },
   },
   plugins: [],
 };
